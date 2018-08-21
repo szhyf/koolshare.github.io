586a7f1450352e96d95819d673efc20f8df2fff3
sadoneli <sadoneli@gmail.com>
2018-08-21 23:40:56 +0800

    update something you know to 4.0.1

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"4.0.0",
-"md5":"6e4ada303e180d1e8b64cd9c222f42fd",
+"version":"4.0.1",
+"md5":"1a2e090fa71e56def7f9340298026f23",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-08-18_19:04:50"
+"build_date":"2018-08-21_23:40:37"
 }
